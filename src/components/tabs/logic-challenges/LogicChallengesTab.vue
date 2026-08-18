33c9999cd1d08ff23f951d9959d47196d42eb93d
aquamarine309 <lantsp05@gmail.com>
2026-08-18 19:56:03 +0800

    fix Logic Chllenge information

--- src/components/tabs/logic-challenges/LogicChallengesTab.vue
+++ src/components/tabs/logic-challenges/LogicChallengesTab.vue
@@ -37,7 +37,7 @@ export default {
 
 <template>
   <div class="l-challenges-tab">
-    <div>Logic Challenges are similar to Normal Challenges: you need to reach a certain amount of Antimatter to complete them. The difference is that you can perform a normal Infinity upon reaching {{ format(inf, 2) }} Antimatter, but you cannot gain Infinity Points within Logic Challenges. Logic Challenges can be run simultaneously with Normal Challenges and Infinity Challenges. Once the requirements of any one of them are met, performing Infinity will complete the corresponding challenge.</div>
+    <div>Logic Challenges are similar to Normal Challenges: you need to reach a certain amount of Antimatter to complete them. The difference is that you can perform a normal Infinity upon reaching {{ format(inf, 2) }} Antimatter, but you cannot gain Infinity Points within Logic Challenges. Logic Challenges can be run simultaneously with Normal Challenges and Infinity Challenges. When they are active together, the goal of the Normal or Infinity Challenge takes priority over the Logic Challenge. If neither a Normal nor an Infinity Challenge is active, the Logic Challenge's own goal will be used instead.</div>
     <div>Logic Challenge 1 is unlocked by a Logic Upgrade. Each subsequent challenge is unlocked after completing the previous one. There are eight challenges in total. Starting from Logic Challenge 2, you need to Break Infinity first.</div>
     <ChallengeTabHeader />
     <ChallengeGrid
